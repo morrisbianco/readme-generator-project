d9d643e6df464d4aace78eca26c029c80dcdb1c0
Andrew Bianco <andrewbianco98@gmail.com>
2021-06-05 17:16:07 -0400

    Added questions

--- index.js
+++ index.js
@@ -1,30 +1,81 @@
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for information about my application repository
-// THEN a high-quality, professional README.md is generated with the title of 
-// my project and sections entitled Description, Table of Contents, Installation, Usage, 
+// GIVEN - application that accepts user input
+// WHEN - prompted for information about my application repository
+// THEN - README.md is generated with title and sections Description, Table of Contents, Installation, Usage, 
 // License, Contributing, Tests, and Questions
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, 
-// Installation, Usage, Contributing, and Tests
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to 
-// the section of the README entitled License that explains which license the application is covered under
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to 
-// reach me with additional questions
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
+// WHEN - enter project title THEN displayed as the title of the README
+// WHEN - enter description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN - information added to README called Description,Installation, Usage, Contributing, and Tests
+// WHEN - choose a license from a list of options
+// THEN - badge for that license added near the top of README and notice is added to README entitled License 
+// that explains which license the application is covered under
+// WHEN - enter my GitHub username THEN added to README entitled Questions, with a link to my GitHub profile
+// WHEN - enter my email address THEN added to README entitled Questions, with instructions to reach me with additional questions
+// WHEN - click on the links in the Table of Contents THEN taken to the corresponding section of the README
 
 
 
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const Choices = require('inquirer/lib/objects/choices');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    inquirer
+        .prompt([
+    {
+        type: "input",
+        message: "What would you like the project title to be?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "What would you like the description to be?",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What are the installation instructions?",
+        name: "instructions"
+    },
+    {
+        type: "input",
+        message: "What is the usage information?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "What are the contribution guidelines?",
+        name: "contribution"
+    },
+    {
+        type: "input",
+        message: "What are the test instructions?",
+        name: "test"
+    },
+    {
+        type: "checkbox",
+        message: "What license would you like to use?",
+        name: "license",
+        choices: [
+            { name: 'MIT', value: 'MIT' },
+            { name: 'Apache 2.0 License', value: 'Apache 2.0 License' },
+            { name: 'GNU GPL v3', value: 'GNU GPL v3' },
+            { name: 'Eclipse Public License 1.0', value: 'Eclipse Public License 1.0' }
+          ],
+    },
+    {
+        type: "input",
+        message: "What is your Github username?",
+        name: "github"
+    },
+    {
+        type: "input",
+        message: "What is your E-mail address?",
+        name: "email"
+    }
+        ])
+        .then()
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
